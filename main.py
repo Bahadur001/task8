02f409657ac6f757dd9aacc09ee1ed02f3dcf34f
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-05-14 23:04:12 +0400

    commit

---
+++ main.py
@@ -0,0 +1,2 @@
+from samitler import samitleri_al
+print("cümlenin samitleri:", samitleri_al(input(" cümle girin: ")))
---
+++ samitler.py
@@ -0,0 +1,2 @@
+def samitleri_al(cumle):
+    return ''.join([herf for herf in cumle if herf.lower() not in 'aeiou'])
